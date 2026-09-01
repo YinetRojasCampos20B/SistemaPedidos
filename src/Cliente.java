d83c670fdfadf53044701ddb3fc9761772700273
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-08-31 20:43:04 -0500

    Definiendo los atributos de la superclase Cliente (nombre, identificación, categoría y edad)

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,29 @@
+/**
+ * SUPERCLASE CLIENTE
+ * **/
+
 public class Cliente {
+
+    // Atributos de la superclase Cliente
+
+    private String nombre;
+    private int identificacion;
+    private char categoria; // Valores 'A' o 'B'
+    private short edad;
+
+    // Constructores de la superclase Cliente
+
+    // Constructor de Cliente que recibe todos los atributos como parámetros (nombre, id, categoria y edad)
+
+    public Cliente (String nombre, int identificacion, char categoria, short edad) {
+
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.categoria = categoria;
+        this.edad = edad;
+
+    }
+
+
+
 }
