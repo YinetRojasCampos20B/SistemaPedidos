ab6c8946d6afb7048b87482c9d6d69416b97ed2b
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-01 18:54:48 -0500

    Declarando los atributos propios de la clase hija ClientePlata, específicamente la probabilidad de cashback, el cashback disponible de este semestre, las compras que ha realizado el usuario en el semestre en curso y la fecha del uúltimo cashback registrado en el sistema

--- src/ClientePlata.java
+++ src/ClientePlata.java
@@ -1,2 +1,22 @@
-public class ClientePlata {
+/*
+
+* CLASE HIJA CLIENTE PLATA
+*
+* */
+
+public class ClientePlata extends Cliente {
+
+    // Atributos de la clase hija ClientePlata
+
+    private double probabilidadCashback;
+    private boolean cashbackDisponibleEsteSemestre;
+    private int comprasEnSemestre;
+    private String fechaUltimoCashback;
+
+    /*
+    * CONSTRUCTORES DE LA CLASE HIJA CLIENTE PLATA
+    * */
+
+
+
 }
