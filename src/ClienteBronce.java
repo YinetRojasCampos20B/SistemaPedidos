09cd3190b036bac5b8ceda681c776b7512330533
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-01 12:01:52 -0500

    Creando un método constructor que reciba como parámetros los atributos de la superclase Cliente, en este caso, nombre, identicación, categoría y edad, mientras que los atributos como pedido gratis disponible se inicializa en true, el descuento acumulado en cero y la fecha del último pedido en No hay pedidos realizados. Este constructor debe ser usado cuando un cliente ordinario pase a ser un ClienteBronce

--- src/ClienteBronce.java
+++ src/ClienteBronce.java
@@ -13,6 +13,10 @@ public class ClienteBronce extends Cliente {
 
     // Constructores de la clase hija ClienteBronce
 
+    /* Constructor que recibe todos los atributos de la superclase Cliente
+    y de la clase hija ClienteBronce
+     */
+
     public ClienteBronce (String nombre, int identificacion, char categoria, short edad, boolean pedidoGratisDisponible, int pedidosDelMes, double descuentoAcumulado, String fechaUltimoPedido) {
         super(nombre, identificacion, categoria, edad);
         this.pedidoGratisDisponible = pedidoGratisDisponible;
@@ -20,5 +24,22 @@ public class ClienteBronce extends Cliente {
         this.descuentoAcumulado = descuentoAcumulado;
         this.fechaUltimoPedido = fechaUltimoPedido;
     }
+
+    /*
+     Constructor que reciba como parámetros los atributos de la superclase Cliente
+    (nombre, identificacion, categoria y edad), pero que va a establecer el pedido gratis como verdadero,
+    el descuento acumulado como 0 y la fecha del último pedido como No hay pedidos realizados
+
+    Va a ser utilizado para cuando el estado de un cliente ordinario se actualice a ClienteBronce
+     */
+
+    public ClienteBronce (String nombre, int identificacion, char categoria, short edad) {
+        super(nombre, identificacion, categoria, edad);
+        pedidoGratisDisponible = true;
+        descuentoAcumulado = 0;
+        fechaUltimoPedido = "No hay pedidos realizados";
+    }
+
+
 
 }
