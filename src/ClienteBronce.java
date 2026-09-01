e15cf0c8b56a5ae9a06274b8cc97958ab960870e
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-01 11:43:24 -0500

    Agregando unn método constructor general dentro de la clase hija ClienteBronce de la superclase Cliente, la cual reciba como parámetros los atributos de la superclase Cliente, en este caso, el nombre, la identificacion, la categoria y la edad, más los atributos propios de la clase hija ClienteBronce, en este caso si tiene un pedido gratis disponible, cuántos pedidos ha hecho en el mes, si tiene un descuento acumulado y la fecha del último pedido realizado

--- src/ClienteBronce.java
+++ src/ClienteBronce.java
@@ -2,7 +2,7 @@
 * CLASE HIJA CLIENTE BRONCE
 */
 
-public class ClienteBronce {
+public class ClienteBronce extends Cliente {
 
     // Atributos de la clase hija ClienteBronce
 
@@ -11,5 +11,14 @@ public class ClienteBronce {
     double descuentoAcumulado;
     String fechaUltimoPedido;
 
+    // Constructores de la clase hija ClienteBronce
+
+    public ClienteBronce (String nombre, int identificacion, char categoria, short edad, boolean pedidoGratisDisponible, int pedidosDelMes, double descuentoAcumulado, String fechaUltimoPedido) {
+        super(nombre, identificacion, categoria, edad);
+        this.pedidoGratisDisponible = pedidoGratisDisponible;
+        this.pedidosDelMes = pedidosDelMes;
+        this.descuentoAcumulado = descuentoAcumulado;
+        this.fechaUltimoPedido = fechaUltimoPedido;
+    }
 
 }
