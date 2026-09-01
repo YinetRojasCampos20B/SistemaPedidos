2e256a641a87f8683570a6c768ebd7767742b017
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-01 11:31:35 -0500

    Asignando los atributos para la clase hija ClienteBronce, en este caso, los siguientes: si tiene un pedido gratis disponible los pedidos que ha realizado durante el mes, el descuento que tiene acumulado y la fecha del último pedido realizado en el sistema

--- src/ClienteBronce.java
+++ src/ClienteBronce.java
@@ -1,2 +1,15 @@
+/*
+* CLASE HIJA CLIENTE BRONCE
+*/
+
 public class ClienteBronce {
+
+    // Atributos de la clase hija ClienteBronce
+
+    boolean pedidoGratisDisponible;
+    int pedidosDelMes;
+    double descuentoAcumulado;
+    String fechaUltimoPedido;
+
+
 }
