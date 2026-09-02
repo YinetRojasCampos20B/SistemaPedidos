436aa15959260fa7c964fa8c09a5dfd82d1068b3
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-02 15:04:58 -0500

    Creando el método constructor general para la clase hija Cliente Dorado, que recibe como parámetros los atributos de la clase madre Cliente (nombre, identificación ,categoría y edad) y todos los atributos de la clase ClienteDorado (pedidos gratis restantes, total de pedidos histórico, monto descuento de compra y si ya usó el beneficio de compra)

--- src/ClienteDorado.java
+++ src/ClienteDorado.java
@@ -11,6 +11,23 @@ public class ClienteDorado extends Cliente {
     private double montoDescuentoCompra;
     private boolean beneficioCompraUsado;
 
+    /*
+    * MÉTODOS CONSTRUCTORES DE LA CLASE HIJA
+    * CLIENTE DORADO
+    * */
 
+    /*
+    * Método constructor que recibe como parámetros los atributos de la clase madre Cliente (nombre, identificación ,categoría y edad) y todos los atributos de la clase ClienteDorado (pedidos gratis restantes, total de pedidos histórico, monto descuento de compra y si ya usó el beneficio de compra).
+    *    Se usa para construir objetos que ya tengan todos estos parámetros.
+    * */
+
+    public ClienteDorado(String nombre, int identificacion, char categoria, short edad, short pedidosGratisRestantes, int totalPedidosHistorico, double montoDescuentoCompra,boolean beneficioCompraUsado) {
+        super(nombre, identificacion, categoria, edad);
+        this.pedidosGratisRestantes = pedidosGratisRestantes;
+        this.totalPedidosHistorico = totalPedidosHistorico;
+        this.montoDescuentoCompra = montoDescuentoCompra;
+        this.beneficioCompraUsado = beneficioCompraUsado;
+
+    }
 
 }
