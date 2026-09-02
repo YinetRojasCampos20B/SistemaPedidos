2495eda4f5678e3e3f8a2f008772a72f687a442a
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-02 14:24:49 -0500

    Creando el segundo método constructor de la clase hija Cliente Plata, subclase de Cliente, la cual como parámetros todos los atributos de la super clase Cliente (nombre, identificación, categoría, edad) y sus atributos propios, tales como la probabilidad de cashback, el cashback disponible este semestre, las compras de este semestre y la fecha del último cashback, y el último método constructor de la misma clase, que recibe como parámetros los atributos de la superclase, específicamente las compras en el semestre y la fecha del último cashback, asignado un valor falso al cashback disponible y cero a la probabilidad de tener cashback

--- src/ClientePlata.java
+++ src/ClientePlata.java
@@ -22,7 +22,7 @@ public class ClientePlata extends Cliente {
     * Método constructor que recibe como parámetros todos los atributos de la super clase Cliente
     *    (nombre, identificación, categoría, edad) y sus atributos propios, tales como la probabilidad
     * de cashback, el cashback disponible este semestre, las compras de este semestre y la fecha del último cashback
-    *
+    * Es usado en el caso de que el cliente ordinario haya sido trasladado a ClientePlata
     *  */
 
     public ClientePlata (String nombre, int identificacion, char categoria, short edad, double probabilidadCashback, boolean cashbackDisponibleEsteSemestre, int comprasEnSemestre, String fechaUltimoCashback) {
@@ -36,15 +36,36 @@ public class ClientePlata extends Cliente {
 
     /*
     *
-    * Método constructor que
-    *
+    * Método constructor que recibe como parámetros todos los atributos de la super clase Cliente
+    * (nombre, identificación, categoría, edad) y los atributos propios de la clase hija  ClientePlata, en este caso,
+    * específicamente las compras en el semestre y la fecha del último cashback, asignando
+    * un valor false al cashback disponible y cero a la probabilidad del cashback
+    * Se usa cuando el cliente Plata ya gastó su cashback
     * */
 
-    public ClientePlata() {
-
-
+    public ClientePlata(String nombre, int identificacion, char categoria, short edad, int comprasEnSemestre, String fechaUltimoCashback) {
+        super(nombre, identificacion, categoria, edad);
+        this.comprasEnSemestre = comprasEnSemestre;
+        this.fechaUltimoCashback = fechaUltimoCashback;
+        probabilidadCashback = 0;
+        cashbackDisponibleEsteSemestre = false;
     }
 
+    /*
+    * Método constructor que recibe como parámetros todos los atributos de la super clase Cliente
+     * (nombre, identificación, categoría, edad) y los atributos propios de la clase Cliente Plata, específicamente
+     * la probabilidad de cashback y la fecha del último cashback realizado
+     * Se usa para instanciar objetos de la clase hija ClientePlata que no tengan
+     * compras hechas este semestre, pero que tengan historial de probabilidad y fecha
+    * */
+
+    public ClientePlata (String nombre, int identificacion, char categoria, short edad, double probabilidadCashback, String fechaUltimoCashback) {
+        super(nombre, identificacion, categoria, edad);
+        this.probabilidadCashback = probabilidadCashback;
+        this.fechaUltimoCashback = fechaUltimoCashback;
+        comprasEnSemestre = 0;
+        cashbackDisponibleEsteSemestre = true;
+    }
 
 
 }
