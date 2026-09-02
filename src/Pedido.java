c861d92daf0fb3adf969336fbadd6e7e93ccd413
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-02 18:21:28 -0500

    Creando el constructor general para la clase Pedido, que recibirá como parámetros el objeto de tipo Cliente, un arreglo de objetos de tipo Producto, la cantidad de productos y la fecha, inicializando el total de la compra en cero

--- src/Pedido.java
+++ src/Pedido.java
@@ -12,6 +12,21 @@ public class Pedido {
     private String fecha;
     private double total;
 
+    /*
+    * CONSTRUCTOR DE LA CLASE PEDIDO
+    * */
 
+    public Pedido(Cliente cliente, Producto[] productos, int cantidadProductos, String fecha) {
+        this.cliente = cliente;
+        this.productos = productos;
+        this.cantidadProductos = cantidadProductos;
+        this.fecha = fecha;
+        total = 0;
+    }
 
+    // Método para calcular el total de una compra hecha por el cliente
+
+    public double calcularTotalCompra(Producto[]) {
+
+    }
 }
