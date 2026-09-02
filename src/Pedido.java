425590ca282c7c3a46a99ad6573ad6871b25e0d3
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-02 18:17:38 -0500

    Definiendo los atributos de la clase Pedido, en este caso, un objeto de la superclase Cliente, un arreglo de la clase Producto, la cantidad de productos, la fecha y el total a pagar

--- src/Pedido.java
+++ src/Pedido.java
@@ -4,5 +4,14 @@
 
 public class Pedido {
 
+    // Atributos de la clase Pedido
+
+    private Cliente cliente;
+    private Producto[] productos;
+    private int cantidadProductos;
+    private String fecha;
+    private double total;
+
+
 
 }
