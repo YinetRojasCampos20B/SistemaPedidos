df42255a318e8dbcbaa6ac29cbb1e451ab10e43b
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-02 15:36:37 -0500

    Generando el msegundo mpétodo constructor de la clase ClienteDorado, en donde se recibe como parámetros los atributos de la clase madre Cliente (nombre, identificación ,categoría y edad) y los atributos de la clase ClienteDorado, como el total de pedidos histórico, el monto descuento de la compra y si ya usó el descuento de compra), estableciendo por defecto los pedidos gratis que le quedan por usar en 0

--- src/ClienteDorado.java
+++ src/ClienteDorado.java
@@ -6,10 +6,10 @@ public class ClienteDorado extends Cliente {
 
     // Atributos de la clase hija ClienteDorado
 
-    private short pedidosGratisRestantes;
-    private int totalPedidosHistorico;
-    private double montoDescuentoCompra;
-    private boolean beneficioCompraUsado;
+    private final short pedidosGratisRestantes;
+    private final int totalPedidosHistorico;
+    private final double montoDescuentoCompra;
+    private final boolean descuentoCompraUsado;
 
     /*
     * MÉTODOS CONSTRUCTORES DE LA CLASE HIJA
@@ -17,17 +17,29 @@ public class ClienteDorado extends Cliente {
     * */
 
     /*
-    * Método constructor que recibe como parámetros los atributos de la clase madre Cliente (nombre, identificación ,categoría y edad) y todos los atributos de la clase ClienteDorado (pedidos gratis restantes, total de pedidos histórico, monto descuento de compra y si ya usó el beneficio de compra).
+    * Método constructor que recibe como parámetros los atributos de la clase madre Cliente (nombre, identificación ,categoría y edad) y todos los atributos de la clase ClienteDorado (pedidos gratis restantes, total de pedidos histórico, monto descuento de compra y si ya usó el descuento de compra).
     *    Se usa para construir objetos que ya tengan todos estos parámetros.
     * */
 
-    public ClienteDorado(String nombre, int identificacion, char categoria, short edad, short pedidosGratisRestantes, int totalPedidosHistorico, double montoDescuentoCompra,boolean beneficioCompraUsado) {
+    public ClienteDorado(String nombre, int identificacion, char categoria, short edad, short pedidosGratisRestantes, int totalPedidosHistorico, double montoDescuentoCompra,boolean descuentoCompraUsado) {
         super(nombre, identificacion, categoria, edad);
         this.pedidosGratisRestantes = pedidosGratisRestantes;
         this.totalPedidosHistorico = totalPedidosHistorico;
         this.montoDescuentoCompra = montoDescuentoCompra;
-        this.beneficioCompraUsado = beneficioCompraUsado;
+        this.descuentoCompraUsado = descuentoCompraUsado;
 
     }
 
+    /*
+    Método constructor que recibe como parámetros los atributos de la clase madre Cliente (nombre, identificación ,categoría y edad) y los atributos de la clase ClienteDorado, como el total de pedidos histórico, el monto descuento de la compra y si ya usó el descuento de compra), estableciendo por defecto los pedidos gratis restantes en cero (0)
+    Se usa cuando los pedidos gratis que tiene el cliente dorado han sido usados durante ese mes
+     */
+
+    public ClienteDorado(String nombre, int identificacion, char categoria, short edad, int totalPedidosHistorico, double montoDescuentoCompra, boolean descuentoCompraUsado) {
+        super(nombre, identificacion, categoria, edad);
+        pedidosGratisRestantes = 0;
+        this.totalPedidosHistorico = totalPedidosHistorico;
+        this.montoDescuentoCompra = montoDescuentoCompra;
+        this.descuentoCompraUsado = descuentoCompraUsado;
+    }
 }
