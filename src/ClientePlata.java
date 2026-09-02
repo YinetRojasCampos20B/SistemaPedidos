5d172cc50772d883104ec07253bea1325d9c0f75
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-01 19:18:44 -0500

    Método constructor que recibe como parámetros todos los atributos de la super clase Cliente (nombre, identificación, categoría, edad) y sus atributos propios, tales como la probabilidad de cashback, el cashback disponible este semestre, las compras de este semestre y la fecha del último cashback

--- src/ClientePlata.java
+++ src/ClientePlata.java
@@ -17,6 +17,23 @@ public class ClientePlata extends Cliente {
     * CONSTRUCTORES DE LA CLASE HIJA CLIENTE PLATA
     * */
 
+    /*
+
+    * Método constructor que recibe como parámetros todos los atributos de la super clase Cliente
+    *    (nombre, identificación, categoría, edad) y sus atributos propios, tales como la probabilidad
+    * de cashback, el cashback disponible este semestre, las compras de este semestre y la fecha del último cashback
+    *
+    *  */
+
+    public ClientePlata (String nombre, int identificacion, char categoria, short edad, double probabilidadCashback, boolean cashbackDisponibleEsteSemestre, int comprasEnSemestre, String fechaUltimoCashback) {
+        super(nombre, identificacion, categoria, edad);
+        this.probabilidadCashback = probabilidadCashback;
+        this.cashbackDisponibleEsteSemestre = cashbackDisponibleEsteSemestre;
+        this.comprasEnSemestre = comprasEnSemestre;
+        this.fechaUltimoCashback = fechaUltimoCashback;
+
+    }
+
 
 
 }
