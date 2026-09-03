a6d83c283ee87b6563a4241be04a107984a2397f
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-03 10:57:56 -0500

    Trabajo realizado en clase de Metodologías de Desarrollo de Software 3-09-26. Subido en este commit por inconvenientes con las credenciales en Git. Se instancian tres objetos de la superclase Cliente, acorde a sus tres métodos constructores correspondientes. Además, se crean dos objetos de la clase hija ClienteBronce, correspondientes a los dos primeros constructores de dicha clase

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,55 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Instanciación de los objetos de la clase Cliente
+        /*
+        INSTANCIACIÓN DE OBJETOS: Superclase Cliente
+         */
 
-        // Instanciación de objetos de Cliente usando el método constructor que hace uso de todos los atributos como parámetros
+        /* Instanciación de objetos de Cliente usando el método constructor que hace
+        uso de todos los atributos como parámetros
+         */
 
-        Cliente cliente1 = new Cliente("Maria", 23456789, 'A', (short) 28);
+        Cliente cliente1 = new Cliente("Maria Martínez", 23456789, 'A', (short) 28);
 
         /* Instanciación de objetos de Cliente, usando el método constructor que recibe el nombre,
          * identificación y edad, sin recibir la categoría
          * y estableciendo 'N' Ninguna por defecto internamente */
 
-        Cliente cliente2 = new Cliente("Fernando", 76543210, (short) 32);
+        Cliente cliente2 = new Cliente("Fernando Fernandez", 76543210, (short) 32);
 
-        //  Imprimir los valores de los atributos de cada objeto creado a partir de la clase Cliente
+        /* Instanciación de objetos de Cliente, usando el método constructor que recibe el nombre,
+         * identificación y edad, sin recibir la edad
+         * y estableciendo cero (0) por defecto internamente */
+
+        Cliente cliente3 = new Cliente( "Emma Mary Tiglao", 657483920, 'B');
+
+        /*
+        INSTANCIACIÓN DE OBJETOS: Clase hija ClienteBronce
+         */
+
+        /* Instanciación de objetos de ClienteBronce usando el método constructor #1,
+        que hace uso de todos los atributos como parámetros (nombre, identificación, categoría, si el pedido
+        gratis está disponible, los pedidos del mes, el descuento acumulado y la fecha del último pedido
+         */
+
+        ClienteBronce clienteBronce1 = new ClienteBronce( "Mario Hernandez", 1234567, 'A', (short) 20, false, 0, 0.15, "04/09/2025");
+
+        /* Instanciación de objetos de ClienteBronce, usando el método constructor #2, recibiendo como parámetros los atributos de la superclase Cliente (nombre, identificacion, categoria y edad), pero que va a establecer internamente el pedido gratis como verdadero, el descuento acumulado como 0 y la fecha del último pedido como No hay pedidos realizados
+        Este es un objeto que representa un cliente ordinario, que recién se encuentra en la membresía de Cliente Bronce
+        * */
+
+        ClienteBronce clienteBronce2 = new ClienteBronce( "Donatella Versace", 1234567, 'A', (short) 68);
+
+        //  Imprimir los valores de los atributos de cada objeto creado a partir de la superclase Cliente
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(cliente3);
+
+        // Imprimir los valores de los atributos de cada objeto creado a partir de la clase hija ClienteBronce
+
+        System.out.println(clienteBronce1);
+        System.out.println(clienteBronce2);
+
     }
 }
